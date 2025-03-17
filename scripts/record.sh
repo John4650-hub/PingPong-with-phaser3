1532092a6bf1863a2b3b579eb722f08a8542f115
john4650-hub <johndelvin51@gmail.com>
2025-03-17 13:47:28 +0300

    gom

--- scripts/record.sh
+++ scripts/record.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python scripts/pyScripts/main.py
 ulimit -s 500000
-ffmpeg -framerate 30 -i downloads/frame%d.png output.mp4
+ffmpeg  -i downloads/frame%d.png output.mp4
 gh release create "$1" output.mp4
