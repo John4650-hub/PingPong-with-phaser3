544004c323656adf66343989177700d629228fdd
john4650-hub <johndelvin51@gmail.com>
2025-03-17 08:36:30 +0300

    done

--- scripts/record.sh
+++ scripts/record.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python scripts/pyScripts/main.py
-ls downloads
+ls */**
 #ffmpeg -framerate 30 -i downloads/frame%d.png -c:v libx264 -r 30 -pix_fmt yuv420p output.mp4
 gh release create "$1" downloads/*.png
