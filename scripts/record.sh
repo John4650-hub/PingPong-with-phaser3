365c858d2ccf2bc80cd6ae9dd36c08c3f5a858ea
john4650-hub <johndelvin51@gmail.com>
2025-03-17 14:22:44 +0300

    done

--- scripts/record.sh
+++ scripts/record.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python scripts/pyScripts/main.py
 ulimit -s 500000
-ffmpeg  -i downloads/frame%d.png output.mp4
+ffmpeg -f image2 -i downloads/frame%d.png output.mp4
 gh release create "$1" -c copy output.mp4
