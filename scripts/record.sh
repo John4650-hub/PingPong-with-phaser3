7a71b85fe75497975c3d97b436b2446abc71a075
john4650-hub <johndelvin51@gmail.com>
2025-03-17 07:32:59 +0300

    jd

--- scripts/record.sh
+++ scripts/record.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 python scripts/pyScripts/main.py
-ls downloads/
-ffmpeg -framerate 30 -i downloads/frame%d.png -c:v libx264 -r 30 -pix_fmt yuv420p output.mp4
-gh release create "$1" output.mp4
+ls "/home/$(whoami)/Downloads"
+
+#ffmpeg -framerate 30 -i downloads/frame%d.png -c:v libx264 -r 30 -pix_fmt yuv420p output.mp4
+gh release create "$1" downloads/*.png
