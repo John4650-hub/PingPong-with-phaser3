57273fabff52fbeab94724bdf15edf04b4a31e38
john4650-hub <johndelvin51@gmail.com>
2025-03-17 15:43:14 +0300

    reverted to use pixl

--- scripts/record.sh
+++ scripts/record.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python scripts/pyScripts/main.py
 ulimit -s 500000
-ffmpeg -f image2 -i downloads/frame%d.png -c copy output.mp4
+ffmpeg -f image2 -framerate 30 -i downloads/frame%d.png -c:v h264_nvenc -preset fast -r 30 -pix_fmt yuv420p output.mp4
 gh release create "$1" output.mp4
