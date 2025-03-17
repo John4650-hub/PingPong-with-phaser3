2242769590a6e754661e0d6daf5d7241297e9a49
john4650-hub <johndelvin51@gmail.com>
2025-03-17 11:54:17 +0300

    done

--- scripts/pyScripts/main.py
+++ scripts/pyScripts/main.py
@@ -34,7 +34,7 @@ try:
     driver.get("http://localhost:8080")
     # Interact with the app (e.g., wait for 2 minutes)
     # List all entries in the directory
-    time.sleep(100)
+    time.sleep(180)
 
 finally:
     # Close the browser
