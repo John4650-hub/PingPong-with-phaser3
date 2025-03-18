5025f00e37e5957e860fbbed8d8ec4c787959396
john4650-hub <johndelvin51@gmail.com>
2025-03-18 05:03:11 +0300

    done

--- scripts/record.sh
+++ scripts/record.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python scripts/pyScripts/main.py
 ulimit -s 500000
-ffmpeg -framerate 24 -i downloads/frame%d.png -c:v libx264 output.mp4
+ffmpeg -framerate 25 -i downloads/frame%d.png -c:v h264_nvenc -preset fast -r 25 -pix_fmt yuv420p output.mp4
 gh release create "$1" output.mp4
