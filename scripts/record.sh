78a6a323480ce13313b91a0d1e918fc6433e6464
john4650-hub <johndelvin51@gmail.com>
2025-03-18 19:17:02 +0300

    done

--- scripts/record.sh
+++ scripts/record.sh
@@ -5,7 +5,7 @@ cd downloads
 imageCount=$(find ./ -type f -name "*.png" | wc -l)
 files=()
 for i in $(seq 1 $imageCount); do
-  files+="frame$i.png"
+  files+="frame$i.png "
 done
 ffmpeg -framerate 30 -i ${files[@]} -c:v h264_nvenc -preset fast -pix_fmt yuv420p output.mp4
 cd ..
