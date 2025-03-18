fb00bdd50c2996f4ee93d4cdb57d58d509348fe4
john4650-hub <johndelvin51@gmail.com>
2025-03-18 18:44:44 +0300

    done

--- scripts/record.sh
+++ scripts/record.sh
@@ -2,7 +2,8 @@
 python scripts/pyScripts/main.py
 ulimit -s 500000
 cd downloads
-ffmpeg -framerate 30 -i frame%d.png -c:v h264_nvenc -preset fast -pix_fmt yuv420p output.mp4
+imageCount=$(find ./ -type f -name "*.png" | wc -l)
+files=(frame{1..$imageCount})
+ffmpeg -framerate 30 -i "${files[@]}" -c:v h264_nvenc -preset fast -pix_fmt yuv420p output.mp4
 cd ..
-
 gh release create "$1" downloads/output.mp4
