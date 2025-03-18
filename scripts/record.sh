d62e8dfb3fb578a9fe41aa0c02210c31fd7e8229
john4650-hub <johndelvin51@gmail.com>
2025-03-18 19:22:58 +0300

    done

--- scripts/record.sh
+++ scripts/record.sh
@@ -7,6 +7,6 @@ files=()
 for i in $(seq 1 $imageCount); do
   files+="frame$i.png "
 done
-ffmpeg -framerate 30 -i ${files[@]} -c:v h264_nvenc -preset fast -pix_fmt yuv420p output.mp4
+ffmpeg -framerate 30 -i "'${files[@]}'" -c:v h264_nvenc -preset fast -pix_fmt yuv420p output.mp4
 cd ..
 gh release create "$1" downloads/output.mp4
